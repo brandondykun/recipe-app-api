de2b699d48b793918c9f22ceab67d2ca99d80ae9
Brandon Dykun <brandon.dykun@barnstormresearch.com>
2023-10-17 10:07:24 -0400

    Completed tag API.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,6 +10,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='example@gmail.com', password='pass123'):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelsTests(TestCase):
     """Test Models."""
 
@@ -67,3 +72,10 @@ class ModelsTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
